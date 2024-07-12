5ac23c885d917e8cc7d7ccfc24bf520d11a3377a
rvveber <8644906+rvveber@users.noreply.github.com>
2024-07-12 17:31:53 +0200

    cake changes

--- src/host/cake/configuration.nix
+++ src/host/cake/configuration.nix
@@ -9,7 +9,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   boot.initrd.luks.devices."luks-71cfbe03-2af1-4e86-b2f9-9e4ca147568b".device = "/dev/disk/by-uuid/71cfbe03-2af1-4e86-b2f9-9e4ca147568b";
   networking.hostName = "cake";
