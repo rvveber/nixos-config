38c6ed5b755b2fd97274957be5c699d40143ddfc
rvveber <robin@we8.dev>
2024-10-01 08:08:12 +0200

    Removed docker from user imports

--- src/user/i/default.nix
+++ src/user/i/default.nix
@@ -2,7 +2,6 @@
   imports = [
     # abstract
     ../../module/add/application/home-manager.nix
-    ../../module/add/application/docker.nix
     ../../module/select/customization/application/neovim/rvveber-nvim
 
     # concrete
