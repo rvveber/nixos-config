c62304c942bbda94a59ec1f32b06d8998ffceda4
rvveber <robin@we8.dev>
2025-10-29 10:32:10 +0100

    Add git properly + git lfs

--- src/module/add/application/development.nix
+++ src/module/add/application/development.nix
@@ -5,17 +5,12 @@
 }: {
   programs.direnv.enable = true;
   environment.systemPackages = with pkgs; [
-    git
     gnumake
-
     cachix
     devenv
-
     koji # cli tool for conventional commits
     git-absorb # https://github.com/tummychow/git-absorb
-
     hurl
-
     # glyphs for development
     nerd-fonts.symbols-only
   ];
@@ -24,4 +19,9 @@
     extra-substituters = https://devenv.cachix.org
     extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=
   '';
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
 }
