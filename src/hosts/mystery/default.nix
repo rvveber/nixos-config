d049ab99ac218d82274fa3cd9a3d323f7bbabe33
rvveber <robin@we8.dev>
2026-01-18 20:34:23 +0100

    Add docker to mystery

--- src/hosts/mystery/default.nix
+++ src/hosts/mystery/default.nix
@@ -1,13 +1,14 @@
 {...}: {
   imports = [
+    # abstract
+    ../../module/host/add/hardware/ssd.nix
+    
+    ../../module/host/select/locale/de_DE
+    ../../module/host/select/application/containerization/docker
+
     # concrete
     ./hardware.nix
     ./disko.nix
     ./customization.nix
-
-    # abstract
-    ../../module/host/add/hardware/ssd.nix
-
-    ../../module/host/select/locale/de_DE
   ];
 }
