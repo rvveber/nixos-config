e0b40280bc7895d43cc1253c4f9ed7d39d937cbc
rvveber <robin@we8.dev>
2026-07-20 11:03:50 +0200

    fixes build issue on friday

--- src/hosts/friday/services/urbackup-client.nix
+++ src/hosts/friday/services/urbackup-client.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   services.urbackup-client = {
     serverIdentsFile = config.sops.secrets.host_friday_urbackup_server_idents.path;
     serverIdentsFileDependencies = ["sops-install-secrets.service"];
--- src/hosts/friday/services/wireguard-client.nix
+++ src/hosts/friday/services/wireguard-client.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   services.wireguard-client.interfaces.offsitevpn = {
     enable = true;
     configFile = config.sops.secrets.host_friday_wireguard_offsitevpn_config.path;
